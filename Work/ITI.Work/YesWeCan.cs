08336a677b9ab8bd80b0332d48924d092b32bd8b
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-03-15 12:28:15 +0100

    First Commit.

---
+++ Work/ITI.Work/YesWeCan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITI.Work
+{
+    public class YesWeCan
+    {
+        public void Work()
+        {
+            Console.WriteLine( "Hello" );
+        }
+    }
+}
---
+++ Work/Tests/ITI.Work.Tests/FirstTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ITI.Work.Tests
+{
+    [TestFixture]
+    public class FirstTests
+    {
+
+        [Test]
+        public void yes_we_can()
+        {
+            var y = new YesWeCan();
+            y.Work();
+        }
+
+        [Test]
+        public void an_integer_can_overflow()
+        {
+            unchecked
+            {
+                int i = int.MaxValue + 1;
+            }
+        }
+
+
+
+    }
+}
