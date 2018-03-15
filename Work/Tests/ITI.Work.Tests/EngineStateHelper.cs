4882523e0791217a1fcb0ecc0f8ee551c33d07d3
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-03-15 17:44:16 +0100

    Bit flags & extension methods.

---
+++ Work/Tests/ITI.Work.Tests/EngineStateHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI.Work.Tests
+{
+    enum EngineState : byte
+    {
+        IsBrake = 1 << 0,
+        IsClutch = 1 << 1,
+        IsRunning = 1 << 2,
+        SpeedMask = (1 << 3 | 1 << 4 | 1 << 5),
+        SpeedShift = 3
+    }
+
+    static class EngineStateHelper
+    {
+        public static bool CheckRunning(this EngineState s)
+        {
+            return (s & EngineState.IsRunning) != 0;
+        }
+
+        public static EngineState StopEngine(this EngineState s)
+        {
+            return s & ~EngineState.IsRunning;
+        }
+
+        public static EngineState StartEngine(EngineState s)
+        {
+            return s | EngineState.IsRunning;
+        }
+
+        public static int GetSpeed(EngineState s)
+        {
+            return (int)(s & EngineState.SpeedMask)
+                            >> (int)EngineState.SpeedShift;
+        }
+
+        public static EngineState SetSpeed(EngineState s, int speed)
+        {
+            if (speed < 0 || speed > 7) throw new ArgumentException("Speed must be between 0 and 7.");
+            speed <<= (int)EngineState.SpeedShift;
+            s &= ~EngineState.SpeedMask;
+            return (EngineState)((int)s | speed);
+        }
+
+    }
+}
--- Work/Tests/ITI.Work.Tests/FirstTests.cs
+++ Work/Tests/ITI.Work.Tests/FirstTests.cs
@@ -12,20 +12,42 @@ namespace ITI.Work.Tests
         public void yes_we_can()
         {
             var y = new YesWeCan();
-            Assert.DoesNotThrow( () => y.Work(), "There is no exception when we work." );
+            Assert.DoesNotThrow(() => y.Work(), "There is no exception when we work.");
             y.Invoking(sut => sut.Work()).Should().NotThrow("There is no exception when we work.");
         }
 
         [Test]
         public void an_integer_can_overflow()
         {
-            unchecked
+            checked
             {
-                int i = int.MaxValue + 1;
+                int i = int.MaxValue + -2;
+                i++;
+                i++;
+                i++;
             }
         }
 
 
 
+        [Test]
+        public void playing_with_bitflags()
+        {
+            EngineState state = EngineState.IsRunning
+                                | EngineState.IsClutch;
+
+            bool isRunning = state.CheckRunning();
+
+            isRunning.Should().BeTrue();
+
+            state = EngineStateHelper.StopEngine(state);
+
+            isRunning = EngineStateHelper.CheckRunning( state );
+            isRunning.Should().BeFalse();
+
+            EngineStateHelper.GetSpeed(state).Should().Be(0);
+
+        }
     }
+
 }
