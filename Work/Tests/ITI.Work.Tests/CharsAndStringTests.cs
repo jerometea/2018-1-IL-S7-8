58cbb1977b0ac8c8ed27a35928e7929a8652d6b1
Olivier Spinelli <olivier.spinelli@invenietis.com>
2018-03-23 09:37:09 +0100

    Naïve vs. Better algotithm.

--- Work/Tests/ITI.Work.Tests/CharsAndStringTests.cs
+++ Work/Tests/ITI.Work.Tests/CharsAndStringTests.cs
@@ -25,19 +25,40 @@ namespace ITI.Work.Tests
         public void removing_diacritics()
         {
             string s = "NoëléàùçÄÎµ";
-            RemoveDiacritics( s ).Should().Be( "NoeleaucAIµ" );
+            RemoveDiacriticsNaïve( s ).Should().Be( "NoeleaucAIµ" );
         }
 
 
         [Test]
-        public void testing_arabic()
+        public void testing_arabic_where_vwels_are_not_Diacritics()
         {
             string s = "الغوطة الشرق";
-            var sR = RemoveDiacritics( s );
-
+            var sR = RemoveDiacriticsNaïve( s );
+            sR.Should().Be( s );
+            sR.Should().NotBeSameAs( s );
         }
 
-        static string RemoveDiacritics( string s )
+        [Test]
+        public void naive_and_better_RemoveDiacritics()
+        {
+            string s = "kjhsdjfh 71652";
+            var sRN = RemoveDiacriticsNaïve( s );
+            sRN.Should().Be( s );
+            sRN.Should().NotBeSameAs( s );
+
+            var sB = RemoveDiacriticsBetter( s );
+            sB.Should().BeSameAs( s );
+
+            var sB2 = RemoveDiacriticsBetter( "â ë l" );
+            sB2.Should().Be( "a e l" );
+        }
+
+        static string RemoveDiacriticsBetter( string s )
+        {
+            throw new NotImplementedException();
+        }
+
+        static string RemoveDiacriticsNaïve( string s )
         {
             var sD = s.Normalize( NormalizationForm.FormD );
             StringBuilder b = new StringBuilder();
